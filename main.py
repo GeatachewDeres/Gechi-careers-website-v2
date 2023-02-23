d20fa8aae599cec31b3d8ce3c6e1f61ccc7d71a6
Getachew Deres <110812677+GeatachewDeres@users.noreply.github.com>
2023-02-23 10:25:55 +0000

    Added HTML and CSS

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from  flask import Flask
+from  flask import Flask, render_template 
 ap= Flask(__name__)
 @ap.route("/")
 def hello_world():
-  return("hello Gechi")
+  return render_template('home.html')
+ # return("hello Gechi")
+  
 if __name__== "__main__": 
 # print("hi every body. i am inside main")
   ap.run(host = '0.0.0.0', debug=True)
